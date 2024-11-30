ea2dc2ee3d720900f2992fd05749e029ff1a2de5
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-11-29 16:04:22 -0800

    Create TreeNode.h

---
+++ Graph Theory/TreeNode.h
@@ -0,0 +1,6 @@
+// Simple binary search tree node declaration that will be used a lot in the other graph theory algorithms so imported from here
+typedef struct {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+} TreeNode;
