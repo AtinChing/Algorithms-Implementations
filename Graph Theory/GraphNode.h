b9b8d88287c4ef86ffd39d2b4a272d2161a36dec
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-11-29 18:01:43 -0800

    Create GraphNode.h

---
+++ Graph Theory/GraphNode.h
@@ -0,0 +1,15 @@
+// Simple binary search tree node declaration that will be used a lot in the other graph theory algorithms so imported from here
+#include <vector>
+class GraphNode {
+public:
+    int val;  // Value or identifier for the node
+    std::vector<GraphNode*> adj;  // List of adjacent nodes (neighbors)
+
+    // Constructor to initialize the node with a value
+    GraphNode(int x) : val(x) {}
+
+    // Add an edge to an adjacent node
+    void addEdge(GraphNode* neighbor) {
+        adj.push_back(neighbor);
+    }
+};
