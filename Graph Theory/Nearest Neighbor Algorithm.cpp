c1d4327d2f43dade25f06ace93adeba53c9fa4cb
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2024-12-30 17:08:58 -0500

    Started nearest neighbor algorithm

---
+++ Graph Theory/Nearest Neighbor Algorithm.cpp
@@ -0,0 +1,58 @@
+#include "Data Structures used/WeightedGraphNode.h"
+#include <vector>
+#include <set>
+#include <iostream>
+#include <unordered_map>
+#include <deque>
+#include <algorithm>
+using namespace std;
+vector<GraphNode*> NearestNeighbor(GraphNode* nodes[], int num_nodes, GraphNode* nodeFrom, GraphNode* nodeTo, set<int> visited = {}){
+    auto curNode = nodeFrom;
+
+    while(curNode){
+        int smalli = 0;
+        int i = 0;
+        for(pair<GraphNode*, int> nodeNext: curNode->adj){
+            if(curNode->adj[smalli].second > nodeNext.second){
+                smalli = i;
+            }
+            i++;
+        }
+
+    }
+
+}
+
+int main(){
+
+    GraphNode* root = new GraphNode(0);
+    GraphNode* B = new GraphNode(1);
+    GraphNode* C = new GraphNode(2);
+    GraphNode* D = new GraphNode(3);
+    GraphNode* E = new GraphNode(4);
+    GraphNode* F = new GraphNode(5);
+    GraphNode* G = new GraphNode(6);
+    GraphNode* H = new GraphNode(7);
+    GraphNode* nodes[] = {root, B, C, D, E, F, G, H};
+    int nodesCount = sizeof(nodes)/sizeof(GraphNode*);
+    // Add the edges
+    root->addEdge(B, 3);
+    root->addEdge(C, 6);
+    B->addEdge(C, 4);
+    B->addEdge(D, 4);
+    B->addEdge(E, 11);
+    C->addEdge(D, 8);
+    C->addEdge(B, 2);
+    C->addEdge(G, 11);
+    D->addEdge(E, -4);
+    D->addEdge(G, 2);
+    D->addEdge(F, 5);
+    E->addEdge(H, 9);
+    F->addEdge(H, 1);
+    G->addEdge(H, 2);
+    auto node1 = root;
+    auto node2 = H;
+    auto res = FloydWarshall(nodes, nodesCount, node1, node2);
+    cout << "Shortest distance between " << node1->val << " and " << node2->val << " is " << res;
+    return 0;
+}
