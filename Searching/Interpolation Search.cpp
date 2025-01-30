c632f7f99d7381d45150b76f8e5223051626c698
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2025-01-30 14:58:50 -0800

    Finished most of interpolation search implementation
    - Still some logic issues to fix as not all elements can be found in the array.

---
+++ Searching/Interpolation Search.cpp
@@ -0,0 +1,44 @@
+/* the interpolation search formula is lo + (((x-arr[lo])*(hi-lo))/(arr[hi]-arr[lo]))
+    where x = element to be searched
+    arr = array which is being searched
+    lo = starting index in arr
+    hi = ending index in arr
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+template <typename T, size_t N>
+int interpolationSearch(T (&arr)[N], T toFind, int low, int high){
+    int length = sizeof(arr)/sizeof(int);
+    if(low <= high && toFind <= arr[high] && toFind >= arr[low]){
+        int pos = low + (((toFind-arr[low])*(high-low))/(arr[high]-arr[low]));
+        if(arr[pos] == toFind){
+            return pos;
+        }
+        else if(arr[pos] < toFind){
+            return interpolationSearch(arr, toFind, low, pos-1);
+        }
+        else if(arr[pos] > toFind){
+            return interpolationSearch(arr, toFind, pos+1, high);
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int test[] = {1, 2, 3, 5, 7, 9, 10};
+    int lookFor;
+    int size = sizeof(test)/sizeof(int);
+    printf("What do you want look for?\n");
+    scanf("%d", &lookFor); /* This will only work searching for ints and strings, template
+    definition would need some form of internal definition of the element/object that is being 
+    searched for in the array we pass in. */
+    int result = interpolationSearch(test, lookFor, 0, size-1);
+    if(result==-1){
+        cout << "Could not find " << lookFor << " in the array!" << endl;
+    }
+    else{
+        cout << "Found " << lookFor << " at " << result << " in the array" << endl;
+    }
+    return 0;
+}
