a6fceaff3c7b99ebe39a275187f90b98a820458e
Atin Kumar Singh <68199228+AtinChing@users.noreply.github.com>
2025-02-02 20:39:19 -0800

    Implemented bulk of TSP dp solving

--- Graph Theory/Travelling Salesman Problem (Dynamic Programming algorithm).cpp
+++ Graph Theory/Travelling Salesman Problem (Dynamic Programming algorithm).cpp
@@ -6,7 +6,7 @@
 #include <deque>
 #include <algorithm>
 using namespace std;
-vector<GraphNode*> TSP(GraphNode* nodes[], int num_nodes, GraphNode* nodeFrom, int* size, set<GraphNode*> visited = {}){
+vector<GraphNode*> TSP(GraphNode* nodes[], int num_nodes, GraphNode* nodeFrom, int* size, vector<vector<int>>& dists, set<GraphNode*> visited = {}){
     vector<vector<int>> dp;
     for(int i = 0; i < num_nodes; i++){
         vector<int> innerrow;
@@ -24,12 +24,36 @@ vector<GraphNode*> TSP(GraphNode* nodes[], int num_nodes, GraphNode* nodeFrom, i
 
     for(int i = 0; i < num_nodes; i++){
         auto node = nodes[i];
-        for(auto subset : combinations(r, N)){
-
+        for(int subset : combinations(i, num_nodes)){
+            if (notIn(nodeFrom->val, subset)){ // we only check combinations for when our start node is included lmao
+                for(int next = 0; next < num_nodes; next++){
+                    auto nextNode = nodes[next];
+                    if(nextNode == nodeFrom || notIn(next, subset)){
+                        auto state = subset ^ (1 << next); // create a state without the next node were going to add
+                        int minDist = INT_MAX;
+                        for (int end = 0; end < num_nodes; end++){
+                            auto endNode = nodes[end];
+                            if(endNode == nodeFrom || endNode == nodeFrom || notIn(end, subset)){
+                                continue;
+                            }
+                            int newDist = dp[end][state] + dists[end][next];
+                            if(newDist < minDist){
+                                minDist = newDist;
+                            }
+                            dp[next][subset] = minDist;
+                        }
+                    }
+                }
+            }
         }
     }
 }
-
+vector<int> combinations(int i, int n){
+    
+}
+bool notIn(int num, int subset){
+    return ((1 << num) & subset) == 0;
+}
 int main(){
     GraphNode* root = new GraphNode(0);
     GraphNode* B = new GraphNode(1);
@@ -41,61 +65,17 @@ int main(){
     GraphNode* H = new GraphNode(7);
     GraphNode* nodes[] = {root, B, C, D, E, F, G, H};
     int nodesCount = sizeof(nodes)/sizeof(GraphNode*);
-
-    // complete graph needed for TSP
-    root->addEdge(B, 3);
-    root->addEdge(C, 6);
-    B->addEdge(C, 4);
-    B->addEdge(D, 4);
-    B->addEdge(E, 11);
-    C->addEdge(D, 8);
-    C->addEdge(B, 2);
-    C->addEdge(G, 11);
-    D->addEdge(E, -4);
-    D->addEdge(G, 2);
-    D->addEdge(F, 5);
-    E->addEdge(H, 9);
-    F->addEdge(H, 1);
-    G->addEdge(H, 2);
-    root->addEdge(D, 7);
-    root->addEdge(E, 9);
-    root->addEdge(F, 12);
-    root->addEdge(G, 14);
-    root->addEdge(H, 10);
-
-    B->addEdge(F, 6);
-    B->addEdge(G, 7);
-    B->addEdge(H, 8);
-
-    C->addEdge(E, 10);
-    C->addEdge(F, 3);
-    C->addEdge(H, 7);
-
-    D->addEdge(B, 6);
-    D->addEdge(C, 5);
-    D->addEdge(H, 4);
-
-    E->addEdge(B, 8);
-    E->addEdge(C, 7);
-    E->addEdge(F, 3);
-    E->addEdge(G, 6);
-
-    F->addEdge(B, 5);
-    F->addEdge(C, 6);
-    F->addEdge(G, 2);
-
-    G->addEdge(B, 4);
-    G->addEdge(E, 1);
-    G->addEdge(F, 5);
-
-    H->addEdge(B, 2);
-    H->addEdge(C, 3);
-    H->addEdge(D, 4);
-    H->addEdge(F, 6);
-    H->addEdge(G, 8);
+    vector<vector<int>> dists;
+        for(int i = 0; i < nodesCount; i++){
+            vector<int> innerrow;
+            for(int j = 0; j < nodesCount; j++){
+                innerrow.push_back(0);
+            }
+            dists.push_back(innerrow);
+        }   
     auto node1 = root;
     int size = 0;
-    auto res = TSP(nodes, nodesCount, node1, &size);
+    auto res = TSP(nodes, nodesCount, node1, &size, dists);
     cout << "Nearest neighbor algorithm path:" << endl;
     for(int i = 0; i < res.size(); i++){
         cout << res[i]->val << " ";
